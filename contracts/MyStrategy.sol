3bad8291b9c1d96090198ca63569083edbf9dc5b
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-23 00:56:26 +0530

    chore: remove unused stuff

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -19,11 +19,9 @@ contract MyStrategy is BaseStrategy {
     IVault public bOxSolid;
 
     bool public withdrawalSafetyCheck = false;
-    bool public harvestOnRebalance = false;
 
     // If nothing is unlocked, processExpiredLocks will revert
     bool public processLocksOnReinvest = false;
-    bool public processLocksOnRebalance = false;
 
     IVlOxd public constant LOCKER = IVlOxd(0xDA00527EDAabCe6F97D89aDb10395f719E5559b9);
 
@@ -34,11 +32,6 @@ contract MyStrategy is BaseStrategy {
 
     // The initial DELEGATE for the strategy // NOTE we can change it by using manualSetDelegate below
     address public constant DELEGATE = address(0x781E82D5D49042baB750efac91858cB65C6b0582);
-
-    // event RewardsCollected(
-    //     address token,
-    //     uint256 amount
-    // );
 
     /// @dev Initialize the Strategy with security settings as well as tokens
     /// @notice Proxies will set any non constant variable you declare as default value
@@ -74,66 +67,11 @@ contract MyStrategy is BaseStrategy {
         withdrawalSafetyCheck = newWithdrawalSafetyCheck;
     }
 
-    ///@dev Should we harvest before doing manual rebalancing
-    ///@notice you most likely want to skip harvest if everything is unlocked, or there's something wrong and you just want out
-    function setHarvestOnRebalance(bool newHarvestOnRebalance) external {
-        _onlyGovernance();
-        harvestOnRebalance = newHarvestOnRebalance;
-    }
-
     ///@dev Should we processExpiredLocks during reinvest?
     function setProcessLocksOnReinvest(bool newProcessLocksOnReinvest) external {
         _onlyGovernance();
         processLocksOnReinvest = newProcessLocksOnReinvest;
     }
-
-    ///@dev Should we processExpiredLocks during manualRebalance?
-    function setProcessLocksOnRebalance(bool newProcessLocksOnRebalance) external {
-        _onlyGovernance();
-        processLocksOnRebalance = newProcessLocksOnRebalance;
-    }
-
-    // /// @dev Function to move rewards that are not protected
-    // /// @notice Only not protected, moves the whole amount using _handleRewardTransfer
-    // /// @notice because token paths are harcoded, this function is safe to be called by anyone
-    // function sweepRewardToken(address token) public {
-    //     _onlyGovernanceOrStrategist();
-    //     _onlyNotProtectedTokens(token);
-    //
-    //     uint256 toSend = IERC20Upgradeable(token).balanceOf(address(this));
-    //     _handleRewardTransfer(token, toSend);
-    // }
-    //
-    // /// @dev Bulk function for sweepRewardToken
-    // function sweepRewards(address[] calldata tokens) external {
-    //     uint256 length = tokens.length;
-    //     for(uint i = 0; i < length; i++){
-    //         sweepRewardToken(tokens[i]);
-    //     }
-    // }
-    //
-    // /// *** Handling of rewards ***
-    // function _handleRewardTransfer(address token, uint256 amount) internal {
-    //     // NOTE: BADGER is emitted through the tree
-    //     if (token == BADGER){
-    //         _sendBadgerToTree(amount);
-    //     } else {
-    //     // NOTE: All other tokens are sent to multisig
-    //         _sentTokenToBribesReceiver(token, amount);
-    //     }
-    // }
-    //
-    // /// @dev Send funds to the bribes receiver
-    // function _sentTokenToBribesReceiver(address token, uint256 amount) internal {
-    //     IERC20Upgradeable(token).safeTransfer(BRIBES_RECEIVER, amount);
-    //     emit RewardsCollected(token, amount);
-    // }
-    //
-    // /// @dev Send the BADGER token to the badgerTree
-    // function _sendBadgerToTree(uint256 amount) internal {
-    //     IERC20Upgradeable(BADGER).safeTransfer(BADGER_TREE, amount);
-    //     emit TreeDistribution(BADGER, amount, block.number, block.timestamp); // TODO
-    // }
 
     /// ===== View Functions =====
 
@@ -256,7 +194,7 @@ contract MyStrategy is BaseStrategy {
 
     // Example tend is a no-op which returns the values, could also just revert
     function _tend() internal override returns (TokenAmount[] memory tended) {
-        revert("no op"); // NOTE: For now tend is replaced by manualRebalance
+        revert("no op");
     }
 
     /// MANUAL FUNCTIONS ///
@@ -286,72 +224,11 @@ contract MyStrategy is BaseStrategy {
         LOCKER.processExpiredLocks(false);
     }
 
-    // function checkUpkeep(bytes calldata checkData) external view returns (bool upkeepNeeded, bytes memory) {
-    //     // We need to unlock funds if the lockedBalance (locked + unlocked) is greater than the balance (actively locked for this epoch)
-    //     upkeepNeeded = LOCKER.lockedBalanceOf(address(this)) > LOCKER.balanceOf(address(this));
-    // }
-    //
-    // /// @dev Function for ChainLink Keepers to automatically process expired locks
-    // function performUpkeep(bytes calldata) external {
-    //     // Works like this because it reverts if lock is not expired
-    //     LOCKER.processExpiredLocks(false);
-    // }
-
     /// @dev Send all available OXD to the Vault
     /// @notice you can do this so you can earn again (re-lock), or just to add to the redemption pool
     function manualSendOXDToVault() external whenNotPaused {
         _onlyGovernance();
         uint256 oxdAmount = balanceOfWant();
         _transferToVault(oxdAmount);
-    }
-
-    /// @dev use the currently available OXD to lock
-    /// @notice toLock = 0, lock nothing, deposit in OXD as much as you can
-    /// @notice toLock = 10_000, lock everything (OXD) you have
-    function manualRebalance(uint256 toLock) external whenNotPaused {
-        _onlyGovernance();
-        require(toLock <= MAX_BPS, "Max is 100%");
-
-        if (processLocksOnRebalance) {
-            // manualRebalance will revert if you have no expired locks
-            LOCKER.processExpiredLocks(false);
-        }
-
-        if (harvestOnRebalance) {
-            _harvest();
-        }
-
-        // Token that is highly liquid
-        uint256 wantBalance = balanceOfWant();
-        // Locked OXD in the locker
-        uint256 balanceInLock = LOCKER.balanceOf(address(this));
-        uint256 totalOXDBalance = wantBalance.add(balanceInLock);
-
-        // Amount we want to have in lock
-        uint256 newLockAmount = totalOXDBalance.mul(toLock).div(MAX_BPS);
-
-        // We can't unlock enough, no-op
-        if (newLockAmount <= balanceInLock) {
-            return;
-        }
-
-        // If we're continuing, then we are going to lock something
-        uint256 oxdToLock = newLockAmount.sub(balanceInLock);
-
-        // We only lock up to the available OXD
-        uint256 maxOXD = balanceOfWant();
-        if (oxdToLock > maxOXD) {
-            // Just lock what we can
-            LOCKER.lock(address(this), maxOXD, getBoostPayment());
-        } else {
-            // Lock proper
-            LOCKER.lock(address(this), oxdToLock, getBoostPayment());
-        }
-
-        // If anything left, send to vault
-        uint256 oxdLeft = balanceOfWant();
-        if (oxdLeft > 0) {
-            _transferToVault(oxdLeft);
-        }
     }
 }
